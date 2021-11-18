4376d75828c85d668a1da33b059734d404108692
taranbis <mikigrm@yahoo.com>
2021-11-18 17:58:14 +0100

    add TCP connection manager

---
+++ util/network/include/tcp_util.hpp
@@ -0,0 +1,28 @@
+#ifndef _TCP_UTIL_HEADER_HPP_
+#define _TCP_UTIL_HEADER_HPP_ 1
+#pragma once
+
+#include <arpa/inet.h>
+
+inline int makeSockAddrAndCallFunction(const std::string &ipAddr, uint16_t port,
+                                    std::function<int(struct sockaddr *, socklen_t)> func)
+{
+    if (ipAddr.find(".") == -1) {
+        struct sockaddr_in6 addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin6_family = AF_INET6;
+        if (inet_pton(AF_INET6, ipAddr.c_str(), &addr.sin6_addr) == 0) { return -1; }
+        addr.sin6_port = htons(port);
+
+        return func((struct sockaddr *)&addr, sizeof(addr));
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, ipAddr.c_str(), &addr.sin_addr) == 0) { return -1; }
+    addr.sin_port = htons(port);
+
+    return func((struct sockaddr *)&addr, sizeof(addr));
+}
+
+#endif
